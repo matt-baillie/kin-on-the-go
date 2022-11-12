148464721b718232c74af38af3e9a9a61eb7d737
Matthew Baillie <mbaillie93@gmail.com>
2022-11-11 23:54:08 -0800

    Installed styled components and react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Routes, BrowserRouter, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ActiveRehab from "./pages/ActiveRehab";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 
 function App() {
   return (
     <div className="App">
-      <h1>Kin On The Go</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/active-rehabilitation" element={<ActiveRehab />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/ActiveRehab.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ActiveRehab = () => {
+  return <div>ActiveRehab</div>;
+};
+
+export default ActiveRehab;
