b0b67fcf7d71f5a05c3cd3a196990b1c3a89cb4c
Matthew Baillie <mbaillie93@gmail.com>
2022-11-11 23:58:48 -0800

    Pages added

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Home from "./pages/Home";
 import ActiveRehab from "./pages/ActiveRehab";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
+import ICBCActive from "./pages/ICBCActive";
+import Testimonials from "./pages/Testimonials";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/active-rehabilitation" element={<ActiveRehab />} />
           <Route path="/about" element={<About />} />
+          <Route path="/icbc" element={<ICBCActive />} />
+          <Route path="/testimonials" element={<Testimonials />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
