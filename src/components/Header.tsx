092fce2ec01d94c6a6eebacea40e686e13ca2da3
Matthew Baillie <mbaillie93@gmail.com>
2022-11-12 20:29:16 -0800

    Header styling, created tasks.txt to track To Do tasks as I think of them

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
+import { StyledHeader } from "./styles/Header.styled";
 
 export const Header = () => {
   return (
-    <header>
+    <StyledHeader>
       <div>Logo</div>
-      <div>
+      <nav>
         <Link to="/our-story">Our Story</Link>
         <Link to="/active-rehabilitation">Active Rehabilitation</Link>
         <Link to="/icbc">ICBC Active Rehab</Link>
         <Link to="/testimonials">Testimonials</Link>
-      </div>
-      <div></div>
-    </header>
+      </nav>
+      <button>Book Now!</button>
+    </StyledHeader>
   );
 };
---
+++ src/components/styles/Header.styled.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  background-color: #fbf;
+  padding: 2rem 0;
+  display: flex;
+  justify-content: space-between;
+
+  nav {
+    display: flex;
+    justify-content: space-between;
+    a {
+      padding: 0 1rem;
+    }
+  }
+
+  button {
+    color: #8a358a;
+    font-size: 1.2rem;
+    background-color: #f9a44d;
+    border-radius: 10px;
+    border: none;
+    padding: 1rem 2rem;
+  }
+`;
