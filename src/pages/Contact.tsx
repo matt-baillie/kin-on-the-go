4cadf8c5c18230f54b85345519523b67691617ad
Matthew Baillie <mbaillie93@gmail.com>
2022-11-13 23:42:15 -0800

    Created Cta and Footer component files. Added assets folder with images

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  const hello = "hello";
+  return <div>Contact {hello}</div>;
 };
 
 export default Contact;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,20 @@ import React from "react";
 import { Container } from "../components/Container.styled";
 
 const Home = () => {
-  return <Container>Home</Container>;
+  return (
+    <Container>
+      <section>Hero</section>
+      <section>Here to help</section>
+      <section>Guarentee</section>
+      <section>About KOTG</section>
+      <section>What is Active Rehabilitation</section>
+      <section>What is a Kinesiologist</section>
+      <section>Pricing</section>
+      <section>Steps</section>
+      <section>Why KOTG?</section>
+      <section>CTA</section>
+    </Container>
+  );
 };
 
 export default Home;
